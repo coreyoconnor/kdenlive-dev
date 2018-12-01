40ff2e5c1a16e3914d5cdacd7496cb7822339a35
Corey O'Connor <coreyoconnor@gmail.com>
2018-12-01 15:50:57 -0800

    ~

--- use-local-src.nix
+++ use-local-src.nix
@@ -1,11 +1,11 @@
 self: super: {
-  kdenlive = (super.kdenlive.overrideAttrs (oldAttrs: rec {
+  kdenlive = super.kdenlive.overrideAttrs (oldAttrs: rec {
     src = self.lib.cleanSource ./kdenlive;
   });
-  mlt = (super.mlt.overrideAttrs (oldAttrs: rec {
+  mlt = super.mlt.overrideAttrs (oldAttrs: rec {
     src = self.lib.cleanSource ./mlt;
   });
-  movit = (super.movit.overrideAttrs (oldAttrs: rec {
+  movit = super.movit.overrideAttrs (oldAttrs: rec {
     src = self.lib.cleanSource ./movit;
   });
 }
